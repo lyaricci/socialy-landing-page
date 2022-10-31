270ba85212dc510819d016fe4855134b894f07dd
Nathálya Ricci <nathalyaricci@gmail.com>
2022-10-31 18:32:34 -0300

    :rocket: finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
-	content: ['./src/**/*.html'],
+	mode: "jit",
+	content: ["./src/**/*.html"],
 	theme: {
 		extend: {
 			fontFamily: {
-				airbnb: ['Airbnb', 'sans-serif'],
-				inter: ['Inter', 'sans-serif'],
+				airbnb: ["Airbnb", "sans-serif"],
+				inter: ["Inter", "sans-serif"],
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/forms")],
 };
